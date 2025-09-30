e375d9c6573fa0b1abba977cd18bee305586dceb
juanCamilo0509 <juank109845@gmail.com>
2025-09-29 22:11:03 -0500

    adding nightly version for neovim

--- easySetup/easySetup.ps1
+++ easySetup/easySetup.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 # Install main applications
-scoop install git neovim cmake ninja clangd main/gcc
+scoop install git neovim-nightly cmake ninja clangd main/gcc
 # Clone neovim config
 git clone https://github.com/juancamilo0509/Snvim.git
 mv Snvim $env:LOCALAPPDATA\nvim
