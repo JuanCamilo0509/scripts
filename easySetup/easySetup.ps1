86a9228135c0ed64f5b3e6a22f2cf02b98a5be11
juanCamilo0509 <juank109845@gmail.com>
2025-10-01 22:44:52 -0500

    Neovim installation bug fix

--- easySetup/easySetup.ps1
+++ easySetup/easySetup.ps1
@@ -1,7 +1,8 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 # Install main applications
-scoop install git neovim-nightly cmake ninja clangd main/gcc
+scoop bucket add versions
+scoop install git versions/neovim-nightly cmake ninja clangd main/gcc
 # Clone neovim config
 git clone https://github.com/juancamilo0509/Snvim.git
 mv Snvim $env:LOCALAPPDATA\nvim
